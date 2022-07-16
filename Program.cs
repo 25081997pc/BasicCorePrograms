be143f11f2b86eb06275d53a9ec61f7d93a182c8
25081997pc <chavanpooja2508@gmail.com>
2022-07-16 18:19:07 +0530

    Printint the Year is Leap or not

--- Program.cs
+++ Program.cs
@@ -4,36 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Number of times Flip The Coin : ");
-            int FlipCoin = Convert.ToInt32(Console.ReadLine());
-            int Head = 0;
-            int Tail = 0;
-            int HeadCount = 0;
-            int TailCount = 0;
-            int HeadPercentage;
-            int TailPercentage;
+            int year;
+            Console.WriteLine("Enter the Four Digit Number Year : ");
+            year = Convert.ToInt32(Console.ReadLine());
 
-            if(FlipCoin > 0)
+            if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
             {
-                for(int i = 1; i <= FlipCoin; i++)
-                {
-                    Random random = new Random();
-                    int num = random.Next(0, 2);
-                    if(num == 1)
-                    {
-                        HeadCount++;
-                    }
-                    else
-                    {
-                        TailCount++;
-                    }
-                    Console.WriteLine(num);
-                }
-                HeadPercentage = HeadCount * 100 / FlipCoin;
-                TailPercentage = TailCount * 100 / FlipCoin;
-                Console.WriteLine("HeadPercentage is : " + HeadPercentage);
-                Console.WriteLine("TailPercentage is : " + TailPercentage);
+                Console.WriteLine("{0} year is Leap Year", year);
             }
+            else
+            {
+                Console.WriteLine("{0} year is not Leap Year", year);
+            }
+            Console.ReadLine();
         }
     }
 }
