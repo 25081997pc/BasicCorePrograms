5637cc490604a4621beaec372a76b588a2dd222e
25081997pc <chavanpooja2508@gmail.com>
2022-07-16 17:55:44 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Solve the Basic Core Programs");          
+        }
+    }
+}
