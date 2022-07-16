8a106f1e557dd4950d6d101af3e5172bbca78b56
25081997pc <chavanpooja2508@gmail.com>
2022-07-16 20:46:31 +0530

    Check Whether a number is Even or Odd

--- Program.cs
+++ Program.cs
@@ -4,19 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the 1st number :");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the 2nd number :");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Before Swap : num1 = " + num1 + " num2 = " + num2);
-
-            num1 = num1 * num2;
-            num2 = num1 / num2;
-            num1 = num1 / num2;
-
-            Console.WriteLine("After Swap : num1 = " + num1 + " num2 = " + num2);
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
         }
     }
 }
