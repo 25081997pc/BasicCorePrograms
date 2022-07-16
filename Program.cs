cc126b5e6171a7fdf48d4a0e054f267c654271be
25081997pc <chavanpooja2508@gmail.com>
2022-07-16 18:01:54 +0530

    Printing Percentage of Heads and Tails

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Solve the Basic Core Programs");          
+            Console.WriteLine("Enter the Number of times Flip The Coin : ");
+            int FlipCoin = Convert.ToInt32(Console.ReadLine());
+            int Head = 0;
+            int Tail = 0;
+            int HeadCount = 0;
+            int TailCount = 0;
+            int HeadPercentage;
+            int TailPercentage;
+
+            if(FlipCoin > 0)
+            {
+                for(int i = 1; i <= FlipCoin; i++)
+                {
+                    Random random = new Random();
+                    int num = random.Next(0, 2);
+                    if(num == 1)
+                    {
+                        HeadCount++;
+                    }
+                    else
+                    {
+                        TailCount++;
+                    }
+                    Console.WriteLine(num);
+                }
+                HeadPercentage = HeadCount * 100 / FlipCoin;
+                TailPercentage = TailCount * 100 / FlipCoin;
+                Console.WriteLine("HeadPercentage is : " + HeadPercentage);
+                Console.WriteLine("TailPercentage is : " + TailPercentage);
+            }
         }
     }
 }
